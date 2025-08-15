0969a511eadf166a260796a795e331abaff766e0
Synergywi <glen@synergywi.com.au>
2025-08-15 17:50:16 +1000

    Add files via upload

--- toolbar.js
+++ toolbar.js
@@ -1,1 +1,11 @@
-// toolbar placeholder
+(function(){
+  // Lightweight toolbar shim so we don't break the rest of the app.
+  // Expose a simple pub/sub for buttons if the host doesn't provide one.
+  if (!window.SynergyBus){
+    const listeners = {};
+    window.SynergyBus = {
+      on(evt, cb){ (listeners[evt] ||= []).push(cb); },
+      emit(evt, data){ (listeners[evt]||[]).forEach(f=>f(data)); }
+    };
+  }
+})();
