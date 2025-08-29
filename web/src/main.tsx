e635ec9aa2391653ff6f3978cf44780704483143
Synergywi <glen@synergywi.com.au>
2025-08-29 17:00:23 +1000

    Update main.tsx

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
-import './tailwind.css'
+
+// Use HubSpot theme only (served from web/public)
+import '/hubspot-theme.css?v7'
 
 const rootEl = document.getElementById('root') as HTMLElement
 createRoot(rootEl).render(
