9d513304361b982eb5abdc0aa264713b3a852400
Synergywi <glen@synergywi.com.au>
2025-08-29 17:26:50 +1000

    Create types.ts

---
+++ web/src/types.ts
@@ -0,0 +1,12 @@
+export type Contact = {
+  id: string
+  givenNames: string
+  surname: string
+  phone?: string
+  email?: string
+  company?: string
+  lastSeen?: string
+  notes?: string
+  createdAt?: string
+  updatedAt?: string
+}
