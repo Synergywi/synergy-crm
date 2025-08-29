761f6bf061298ab462ca4036ef6aa13481dae339
Synergywi <glen@synergywi.com.au>
2025-08-29 18:36:35 +1000

    Update main.tsx

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -3,10 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 
-// Use HubSpot theme only (served from web/public)
-import '/hubspot-theme.css?v7'
-
-const rootEl = document.getElementById('root') as HTMLElement
+const rootEl = document.getElementById('root')!
 createRoot(rootEl).render(
   <React.StrictMode>
     <BrowserRouter>
