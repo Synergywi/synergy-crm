96125de12d59524b0fd1a75a7931315d431cde22
Synergywi <glen@synergywi.com.au>
2025-09-01 08:27:01 +1000

    Update main.tsx

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,10 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+// web/src/main.tsx
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const root = document.getElementById("root")!;
+createRoot(root).render(
+  <StrictMode>
+    {/* Router wrapper prevents the useContext(null) crash */}
+    <BrowserRouter
+      future={{
+        v7_startTransition: true,
+        v7_relativeSplatPath: true,
+      }}
+    >
+      <App />
+    </BrowserRouter>
+  </StrictMode>
 );
