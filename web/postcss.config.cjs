0aa399635970c70d4070b754cca5e1f86ed8a590
Synergywi <glen@synergywi.com.au>
2025-09-02 08:06:29 +1000

    Update postcss.config.cjs

--- web/postcss.config.cjs
+++ web/postcss.config.cjs
@@ -1,7 +1,6 @@
-// web/postcss.config.cjs
 module.exports = {
-  plugins: [
-    require('postcss-import'),
-    require('autoprefixer')
-  ]
+  plugins: {
+    "postcss-import": {},
+    autoprefixer: {}
+  }
 };
