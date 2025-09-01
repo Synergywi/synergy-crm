ce1f6ea66c07c4533beb1560a0517d1c3f211ace
Synergywi <glen@synergywi.com.au>
2025-09-02 08:06:44 +1000

    Update vite.config.ts

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,14 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  root: '.',                  // app is rooted at /web
-  build: {
-    outDir: 'dist',
-    sourcemap: false
-  },
-  server: {
-    port: 5173
-  }
+  base: "/",
+  build: { outDir: "dist" }
 });
